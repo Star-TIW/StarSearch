e4fd3a1839b8028936d3305b975d1cd1b88b57dd
Star <125516384+Star-TIW@users.noreply.github.com>
2023-09-20 17:51:56 +0000

    Create uv.config.js

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/star/search/',
+    bare: 'https://fireworks.sprk.lol/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
