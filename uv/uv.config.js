d8107b7337805e4ff7223869cd41b77b997136e1
Star <125516384+Star-TIW@users.noreply.github.com>
2023-09-28 08:36:56 -0700

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/uv/service/',
+    prefix: '/star/',
     bare: 'https://fireworks.sprk.lol/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
